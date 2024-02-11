6ff75d411b93e25a01f3a15ad7d456e07355f89f
tyler <73712505+InfiniteTimeZ@users.noreply.github.com>
2024-02-10 19:34:22 -0500

    Functioning reset & change size button. Moving onto color dropdown

--- Script/script.js
+++ Script/script.js
@@ -1,13 +1,29 @@
 const grid = document.querySelector("#innerBox");
-const getNum = ("Grid num");
+const changeSize = document.querySelector("#chngS")
+let getNum = 0;
+
+document.getElementById("resetBT").addEventListener('click', function(){
+    document.querySelectorAll('.trR').forEach(e => e.remove());
+});
+
+document.getElementById("chngS").addEventListener('click', function(){
+    document.querySelectorAll('.trR').forEach(e => e.remove());
+    getNum = prompt("Pick a number 2-64");
+    createGrid();
+
+});
 
 function createGrid(){
+  if (getNum < 64 || getNum > 2){  
     for(i = 0; i < getNum; i++){
         const tr = document.createElement('tr');
+        tr.classList.add("trR")
+
     for (j = 0; j < getNum; j++){
         const td = document.createElement('td');
 
         td.setAttribute('style', 'border:1px solid black');
+
 
         tr.append(td);
 
@@ -15,5 +31,5 @@ function createGrid(){
      grid.appendChild(tr);
     }
 }
-createGrid();
+}
 
