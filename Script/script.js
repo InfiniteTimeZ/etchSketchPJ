e1d621c65f19f6d156c6956735073b2d01ef1e41
tyler <73712505+InfiniteTimeZ@users.noreply.github.com>
2024-02-10 21:29:55 -0500

    Styling touch ups working etch sketch gover and id changing finish tmrw

--- Script/script.js
+++ Script/script.js
@@ -2,13 +2,14 @@ const grid = document.querySelector("#tbBox");
 const changeSize = document.querySelector("#chngS")
 let getNum = 0;
 
+
 document.getElementById("resetBT").addEventListener('click', function(){
     document.querySelectorAll('.trR').forEach(e => e.remove());
 });
 
 document.getElementById("chngS").addEventListener('click', function(){
     document.querySelectorAll('.trR').forEach(e => e.remove());
-    getNum = prompt("Pick a number 2-64");
+    getNum = prompt("Pick a number 2-100");
     createGrid();
 
 });
@@ -23,10 +24,14 @@ function createGrid(){
         const td = document.createElement('td');
 
         td.setAttribute('style', 'border:1px solid black');
+        td.classList.add("tdH")
 
 
         tr.append(td);
-
+        
+        td.addEventListener('mouseover', function(){
+            td.setAttribute('style', "background-color: black")
+        });
     }
      grid.appendChild(tr);
     }
